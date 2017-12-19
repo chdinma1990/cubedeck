6fb87926d065357c514eebf24ed13b164571cd08
Chdinma1990 <nonsowinner@gmail.com>
2017-12-19 06:55:28 -0600

    added avatar column to profiles table

---
+++ db/migrate/20171219125041_add_attachment_avatar_profiles.rb
@@ -0,0 +1,12 @@
+class AddAttachmentAvatarProfiles < ActiveRecord::Migration[5.1]
+  def self.up
+  	change_table :profiles do |t|
+  		t.attachment :avatar
+  end
+end
+
+def self.down
+	remove_attachment :profiles, :avatar
+ end
+end
+
